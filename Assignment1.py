6906bb5f5373538230b50a4ed586addbc0aebbac
Hensonam23 <amhenson@student.rtc.edu>
2024-10-19 09:33:02 -0700

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,85 @@
+# This is my last assignment for CNE330. It prints the lyrics to one of our favorite songs.
+# Author: Aaron Henson, amhenson@student.rtc.edu
+# Creation Date: Oct 09, 2024
+
+def Intro():
+    print("This is the greatest and best song in the world…")
+    print("…tribute")
+
+def Verse1():
+    print("Long time ago, me and my brother Kyle here")
+    print("We was hitchhiking down a long and lonesome road")
+    print("All of a sudden, there shined a shiny demon")
+    print("In the middle of the road")
+    print("And he said, “Play the best song in the world")
+    print("Or I'll eat your soul” (Soul)")
+
+def Verse2():
+    print("Well, me and Kyle, we looked at each other")
+    print("And we each said, “Okay”")
+    print("And we played the first thing that came to our heads, just so happened to be")
+    print("The best song in the world, it was the best song in the world")
+
+def Chorus1():
+    print("Look into my eyes and it's easy to see")
+    print("One and one make two, two and one make three")
+    print(" It was destiny")
+    print("Once every hundred-thousand years or so")
+    print("When the sun doth shine and the moon doth glow")
+    print("And the grass doth grow-ho")
+
+def Verse3():
+    print("Needless to say, the beast was stunned")
+    print("A whip-crack went his whippy tail and the beast was done")
+    print("He asked us *snort* “Be you angels?”")
+    print("And we said, “Nay, we are but men!” Rock")
+    print("Ahhh, ahhh, ahhh-ah-ah")
+    print("Ohhh, whoah")
+    print("Ah-whoah-oh")
+
+def Chorus2():
+    print("This is not the greatest song in the world, no")
+    print("This is just a tribute")
+    print("Couldn't remember the greatest song in the world, no")
+    print("No, this is a tribute, oh")
+    print("To the greatest song in the world, alright")
+    print("It was the greatest song in the world, alright")
+    print("Yeah, it was the best motherfucking song")
+    print("The greatest song in the world")
+
+def Bridge():
+    print("Ah-rah, dee")
+    print("Soo-guh-goo-gee-goo-gee")
+    print("Goo-guh fli-goo gee-goo")
+    print("Guh fli-goo, ga-goo-buh-dee")
+    print("Ooh, guh-goo-bee")
+    print("Ooh-guh-guh-bee-guh-guh-bee")
+    print("Fli-goo gee-goo")
+    print("A-fliguh woo-wa mama Lucifer!")
+
+def Interlude():
+    print("And the peculiar thing is this, my friends")
+    print("The song we sang on that fateful night")
+    print("It didn't actually sound anything like this song")
+
+def Outro():
+    print("This is just a tribute, you've gotta believe me")
+    print("And I wish you were there, just a matter of opinion")
+    print("Ah, fuck! Good God, God lovin'")
+    print("So surprised to find you can't stop it")
+    print("A fiery ring of fire, rich motherfucker, aw")
+    print("Alright")
+    print("Alright")
+
+def play_song():
+    Intro()
+    Verse1()
+    Verse2()
+    Chorus1()
+    Verse3()
+    Chorus2()
+    Bridge()
+    Interlude()
+    Outro()
+
+play_song()
